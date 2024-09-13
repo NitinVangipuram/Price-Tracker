c771adfb76c451abfc97a86544cff70e12ca305c
NitinVangipuram <nitin.vangipuram@gmail.com>
2024-09-13 20:10:08 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -300,65 +300,6 @@ app.post("/delete-product", (req, res) => {
     });
   }
 });
-app.post("/update-product", (req, res) => {
-  try {
-    if (req.files && req.body && req.body.name && req.body.desc && req.body.price &&
-      req.body.id && req.body.discount) {
-
-      product.findById(req.body.id, (err, new_product) => {
-
-        // if file already exist than remove it
-        if (req.files && req.files[0] && req.files[0].filename && new_product.image) {
-          var path = `./uploads/${new_product.image}`;
-          fs.unlinkSync(path);
-        }
-
-        if (req.files && req.files[0] && req.files[0].filename) {
-          new_product.image = req.files[0].filename;
-        }
-        if (req.body.name) {
-          new_product.name = req.body.name;
-        }
-        if (req.body.desc) {
-          new_product.desc = req.body.desc;
-        }
-        if (req.body.price) {
-          new_product.price = req.body.price;
-        }
-        if (req.body.discount) {
-          new_product.discount = req.body.discount;
-        }
-
-        new_product.save((err, data) => {
-          if (err) {
-            res.status(400).json({
-              errorMessage: err,
-              status: false
-            });
-          } else {
-            res.status(200).json({
-              status: true,
-              title: 'Product updated.'
-            });
-          }
-        });
-
-      });
-
-    } else {
-      res.status(400).json({
-        errorMessage: 'Add proper parameter first!',
-        status: false
-      });
-    }
-  } catch (e) {
-    res.status(400).json({
-      errorMessage: 'Something went wrong!',
-      status: false
-    });
-  }
-});
-/*Api to get and search product with pagination and search by name*/
 app.get("/get-product", (req, res) => {
   try {
     var query = {};
@@ -367,13 +308,9 @@ app.get("/get-product", (req, res) => {
       is_delete: false,
       user_id: req.user.id
     });
-    if (req.query && req.query.search) {
-      query["$and"].push({
-        name: { $regex: req.query.search, $options: 'i' } // Added case-insensitive search
-      });
-    }
+    console.log(query);
     // Removed pagination logic
-    product.find(query, { date: 1, name: 1, id: 1, desc: 1, price: 1, discount: 1, image: 1 ,productUrl:1})
+    product.find(query)
       .then((data) => {
         if (data && data.length > 0) {
           res.status(200).json({
